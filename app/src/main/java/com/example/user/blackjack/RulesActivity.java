3dcf4a078681d109d83fc92388cadc8af90d920f
Matthew Shield <matthewshield@hotmail.co.uk>
2017-08-15 12:16:31 +0100

    Created Rules Activity, edited layout

--- app/src/main/java/com/example/user/blackjack/RulesActivity.java
+++ app/src/main/java/com/example/user/blackjack/RulesActivity.java
@@ -2,12 +2,26 @@ package com.example.user.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RulesActivity extends AppCompatActivity {
+
+    TextView rules;
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        rules = (TextView) findViewById(R.id.rules);
+        startButton = (Button) findViewById(R.id.start_button);
     }
+
+    public void onStartButtonClicked(View start_button) {
+        
+    }
+
 }
